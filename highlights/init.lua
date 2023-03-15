d174ab877d092ec5d02d6b22eea9d7c0a33d411e
parK-dev <kirk.paradis@gmail.com>
2023-03-16 06:15:32 +0900

    Backgrounds and stuff

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,3 +1,29 @@
-return { -- this table overrides highlights in all themes
-  -- Normal = { bg = "#000000" },
+-- this table overrides highlights in all themes
+return {
+  -- Telescope
+  TelescopeBorder = { bg = "none" },
+  TelescopeNormal = { bg = "none" },
+  -- NeoTree
+  NeoTreeNormal = { bg = "none" },
+  NeoTreeNormalNC = { bg = "none" },
+  -- StatusLine
+  StatusLine = { bg = "none" },
+  StatusLineNC = { bg = "none" },
+  StatusLineTerm = { bg = "none" },
+  StatusLineTermNC = { bg = "none" },
+  -- TabLine
+  TabLine = { bg = "none" },
+  TabLineSel = { bg = "none" },
+  TabLineFill = { bg = "none" },
+  -- Cursor
+  CursorLineNr = { bg = "none" },
+  CursorLine = { bg = "none" },
+  -- Search
+  Search = { fg = 'red' },
+  IncSearch = { fg = 'red' },
+  Pmenu = { bg = "none" },
+  PmenuSel = { bg = "none" },
+  PmenuSbar = { bg = "none" },
+  PmenuThumb = { bg = "none" },
 }
+-- https://github.com/folke/tokyonight.nvim/blob/467d889ba82a74e26821c812aa8e70d7dff04c6c/lua/tokyonight/theme.lua
